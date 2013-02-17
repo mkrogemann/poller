0dede74db9baf03bdff0ef7aab9abe8fc23be70f
Markus Krogemann <markus@krogemann.de>
2013-02-17 14:59:13 +0100

    fix namespaces

--- lib/poller/timeout.rb
+++ lib/poller/timeout.rb
@@ -1,15 +1,17 @@
-class Timeout
+module Poller
+  class Timeout
 
-  # Specify the timeout period in Integer or Floating Point representation
-  # +period_s+:: The timeout period in seconds
-  def initialize(period_s)
-    @period = period_s
-    @start_time = Time.now
+    # Specify the timeout period in Integer or Floating Point representation
+    # +period_s+:: The timeout period in seconds
+    def initialize(period_s)
+      @period = period_s
+      @start_time = Time.now
+    end
+
+    # Returns true if timeout period has elapsed, false if not
+    def occured?
+      Time.now - @start_time >= @period
+    end
+
   end
-
-  # Returns true if timeout period has elapsed, false if not
-  def occured?
-    Time.now - @start_time >= @period
-  end
-
 end
--- spec/poller/timeout_spec.rb
+++ spec/poller/timeout_spec.rb
@@ -1,18 +1,20 @@
 require 'spec_helper'
 require 'poller/timeout'
 
-describe Timeout do
+module Poller
+  describe Timeout do
 
-  context '#occured?' do
+    context '#occured?' do
 
-    it 'returns true if timeout period has expired' do
-      Timeout.new(-1.8).occured?.should be_true
-    end
+      it 'returns true if timeout period has expired' do
+        Timeout.new(-1.8).occured?.should be_true
+      end
 
-    it 'returns false before timeout period has expired' do
-      Timeout.new(Float::MAX).occured?.should be_false
+      it 'returns false before timeout period has expired' do
+        Timeout.new(Float::MAX).occured?.should be_false
+      end
+
     end
 
   end
-
 end
