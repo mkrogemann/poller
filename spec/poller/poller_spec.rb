08e6c2310f121f1a7848497e067bf206397224f0
Markus Krogemann <markus@krogemann.de>
2013-02-18 22:34:08 +0100

    specify exactly how many times probe.satisfied? should get called

--- spec/poller/poller_spec.rb
+++ spec/poller/poller_spec.rb
@@ -22,10 +22,9 @@ module Poller
     context '#check' do
 
       it 'succeeds after 1 poll' do
-        probe.stub(:satisfied?).and_return(true)
-
         # we want to test that the sleep method never gets executed
         Kernel.should_not_receive(:sleep)
+        probe.should_receive(:satisfied?).once.and_return(true)
 
         # we set timeout_s and period_s to nil as they are out of this example's scope. no need
         # to use the timeout mock here as it will not get called because first poll already succeeds
@@ -37,8 +36,9 @@ module Poller
         probe.stub(:satisfied?).and_return(false, false, true)
         timeout.stub(:occured?).and_return(false, false, false)
 
-        # sleep should have been called twice
+        # sleep should have been called twice, probe.satisfied? thrice
         Kernel.should_receive(:sleep).twice
+        probe.should_receive(:satisfied?).exactly(3).times
 
         # we set timeout_s to nil as it is out of this example's scope (and because we mock it)
         poller = PollerModuleHolder.new(probe, nil, 0.0001)
@@ -54,6 +54,7 @@ module Poller
 
         # sleep should have been called once
         Kernel.should_receive(:sleep).once
+        probe.should_receive(:satisfied?).exactly(:twice)
 
         # we set timeout_s to nil as it is out of this example's scope (and because we mock it)
         poller = PollerModuleHolder.new(probe, nil, 0.0001)
