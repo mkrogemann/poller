9e05b4494242c4bd46a401871c59bba9bf28ccb3
Markus Krogemann <markus@krogemann.de>
2013-03-04 19:27:59 +0100

    improve rdoc

--- lib/matchers/http/response_body_contains.rb
+++ lib/matchers/http/response_body_contains.rb
@@ -1,15 +1,20 @@
-# This class expects a Net::HTTPResponse object and a second argument in its constructor.
-# The second argument can be a String or a Regexp
+# This class expects a search_term in its constructor which can be
+# either a String or a Regexp
+#
+# The matches? method takes a Net::HTTPResponse object and will apply
+# the search_term to the HTTPResponse.body
 
 module Matchers
   module HTTP
 
     class ResponseBodyContains
 
+      # Expects either a String or a Regexp
       def initialize(search_term)
         @search_term = search_term
       end
 
+      # Expects a Net::HTTPResponse object
       def matches?(http_response)
         return @search_term.match(http_response.body) if @search_term.class == Regexp
         http_response.body.include?(@search_term)
