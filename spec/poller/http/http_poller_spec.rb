88bb484d7205f324d10d4cc4ff4e74b38b313281
Markus Krogemann <markus@krogemann.de>
2013-03-29 01:09:06 +0100

    adjust integration tests to the fact that sometimes the latencies may be quite high

--- spec/poller/http/http_poller_spec.rb
+++ spec/poller/http/http_poller_spec.rb
@@ -50,7 +50,7 @@ module Poller
 
         it 'succeeds in fetching and matching an http response from example.com', :type => 'integration' do
           matcher = Matchers::HTTP::ResponseBodyContains.new('<title>Example Domain</title>')
-          poller = HttpPoller.new("http://www.iana.org/domains/example", matcher, 3.0, 1.0)
+          poller = HttpPoller.new("http://www.iana.org/domains/example", matcher, 5.0, 1.0)
 
           poller.check.should be_nil
         end
@@ -59,7 +59,7 @@ module Poller
 
         it 'succeeds in fetching an XML document and in finding a text for given XPath', :type => 'integration' do
           matcher = Matchers::XML::XPathContainsText.new('/CATALOG/CD/TITLE', 'Empire Burlesque')
-          poller = HttpPoller.new("http://www.w3schools.com/xml/cd_catalog.xml", matcher, 3.0, 1.0)
+          poller = HttpPoller.new("http://www.w3schools.com/xml/cd_catalog.xml", matcher, 5.0, 1.0)
 
           poller.check.should be_nil
         end
@@ -68,19 +68,19 @@ module Poller
 
         it 'succeeds in fetching an XML document and in finding a given XPath at least given number of times', :type => 'integration' do
           matcher = Matchers::XML::DocumentContainsXPath.new('/CATALOG/CD/ARTIST', 11)
-          poller = HttpPoller.new("http://www.w3schools.com/xml/cd_catalog.xml", matcher, 3.0, 1.0)
+          poller = HttpPoller.new("http://www.w3schools.com/xml/cd_catalog.xml", matcher, 5.0, 1.0)
 
           poller.check.should be_nil
         end
 
         # have a failing test to validate error message
         it 'fails to find a given XPath in document', :type => 'integration' do
-          matcher = Matchers::XML::DocumentContainsXPath.new('/CATALOG/NOT_THERE/ARTIST', 11)
-          poller = HttpPoller.new("http://www.w3schools.com/xml/cd_catalog.xml", matcher, 3.0, 1.0)
+          matcher = Matchers::XML::DocumentContainsXPath.new('/CATALOG/NOT_THERE/LIGHT', 11)
+          poller = HttpPoller.new("http://www.w3schools.com/xml/plant_catalog.xml", matcher, 5.0, 1.0)
 
           expect {
             poller.check
-          }.to raise_error(RuntimeError, /^Timeout period has been exceeded for Poller \(http:\/\/www.w3schools.com\/xml\/cd_catalog.xml\)\. Poller tried 3 times which in total took \d\.?\d* seconds\.$/)
+          }.to raise_error(RuntimeError, /^Timeout period has been exceeded for Poller \(http:\/\/www.w3schools.com\/xml\/plant_catalog.xml\)\. Poller tried \d times which in total took \d\.?\d* seconds\.$/)
         end
 
       end
