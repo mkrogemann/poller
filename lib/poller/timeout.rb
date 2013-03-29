9757e123a5af44fa9915d8ea3a63a3dc824caf8d
Markus Krogemann <markus@krogemann.de>
2013-03-29 02:57:08 +0100

    improve variable names

--- lib/poller/timeout.rb
+++ lib/poller/timeout.rb
@@ -2,9 +2,9 @@ module Poller
   class Timeout
 
     # Specify the timeout period in Integer or Floating Point representation
-    # +period_s+:: The timeout period in seconds
-    def initialize(period_s)
-      @period = period_s
+    # +period_seconds+:: The timeout period in seconds
+    def initialize(period_seconds)
+      @period = period_seconds
       @start_time = Time.now
     end
 
