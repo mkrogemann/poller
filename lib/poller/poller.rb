3f9d5cdb70e17752a531cb4cc3dda8868aa59a7c
Markus Krogemann <markus@krogemann.de>
2013-03-29 02:11:49 +0100

    make sleep_time more testable

--- lib/poller/poller.rb
+++ lib/poller/poller.rb
@@ -15,7 +15,7 @@ module Poller
     def initialize(probe, timeout_s, period_s, name = nil)
       @probe = probe
       @timeout_s = timeout_s
-      @period_s = period_s
+      @period = period_s.to_i
       @name = name.nil? ? "no name given" : name
     end
 
@@ -23,16 +23,16 @@ module Poller
       @timeout ||= Timeout.new(@timeout_s) # allow injecting a timeout from within tests
       @tries = 0
       @check_started_at = Time.now
-      @last_sample_took = 0
+      last_sample_took = 0
 
       while !@timeout.occured?
-        Kernel.sleep sleep_time
+        Kernel.sleep sleep_time(@period, last_sample_took)
         sample_started_at = Time.now
         @probe.sample
         satisfied = @probe.satisfied?
-        @last_sample_took = Time.now - sample_started_at
-        return if satisfied
+        last_sample_took = Time.now - sample_started_at
         @tries += 1
+        return if satisfied
       end
 
       raise RuntimeError, "Timeout period has been exceeded for Poller (#{@name})." \
@@ -40,8 +40,8 @@ module Poller
     end
 
     private
-    def sleep_time
-      st = @period_s.to_i - @last_sample_took
+    def sleep_time(period_i, last_sample_took)
+      st = period_i - last_sample_took
       st < 0 ? 0 : st
     end
 
