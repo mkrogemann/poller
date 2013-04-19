e9a9a8265882679db4393d3073e6d52cb0854f5c
Markus Krogemann <markus@krogemann.de>
2013-04-19 20:43:19 +0200

    refactor tests

--- spec/matchers/http/response_body_contains_spec.rb
+++ spec/matchers/http/response_body_contains_spec.rb
@@ -3,31 +3,27 @@ require 'matchers/http/response_body_contains'
 
 module Matchers
   module HTTP
-
     describe ResponseBodyContains do
+      describe '#matches?' do
+        context 'search term contained in document' do
+          it 'true if search String is contained in String passed to matches? method' do
+            rbc = ResponseBodyContains.new('ng to read cha')
+            rbc.matches?("some too long to read char noise").should be_true
+          end
 
-      context '#matches?' do
-
-        it 'returns true if the search String is contained in String passed to matches? method' do
-          rbc = ResponseBodyContains.new('ng to read cha')
-
-          rbc.matches?("some too long to read char noise").should be_true
+          it 'true if given Regexp matches given Sting' do
+            rbc = ResponseBodyContains.new(/oo/)
+            rbc.matches?("some too long to read char noise").should be_true
+          end
         end
 
-        it 'matches a given Regexp with given Sting (happy case)' do
-          rbc = ResponseBodyContains.new(/oo/)
-
-          rbc.matches?("some too long to read char noise").should be_true
-        end
-
-        it 'matches a given Regexp with given String (no match case)' do
-          rbc = ResponseBodyContains.new(/o ln/)
-
-          rbc.matches?("some too long to read char noise").should be_false
+        context 'search term not contained in document' do
+          it 'false if given Regexp does not match given String' do
+            rbc = ResponseBodyContains.new(/o ln/)
+            rbc.matches?("some too long to read char noise").should be_false
+          end
         end
       end
-
     end
-
   end
 end
