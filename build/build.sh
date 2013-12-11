9fc9fc7f47c8c36fc2d93dd7c4cc6bddab604af5
Markus Krogemann <markus@krogemann.de>
2013-12-12 00:40:48 +0100

    update required

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,7 @@
 # this script is here to be used by Go and similar CI/CD products. It may at some point be replaced by a Rakefile.
 
 rvm use 1.9.3@mkrogemann-poller --create --fuzzy
-bundle
+bundle update
 [ -d rspec ] && rm -rf rspec
 COVERAGE=true bundle exec rspec --tag ~type:integration --out rspec/rspec.xml --format html --out rspec/rspec.html
 
